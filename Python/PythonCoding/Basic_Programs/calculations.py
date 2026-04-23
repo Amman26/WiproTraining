2e7ff157ea514cd9a3c919a4013754d938658047
Amman26 <ammanfarooque@gmail.com>
2026-04-23 09:45:15 +0530

    Added some new Python programs

---
+++ Python/PythonCoding/Basic_Programs/calculations.py
@@ -0,0 +1,8 @@
+a = int(input('enter a number :'))
+b = int(input('enter another number :'))
+print("sum : ",a + b)
+print("diff : ",a - b)
+print("prod : ",a * b)
+print("int div : ",a / b)
+print("floor div: ",a // b)
+print('mod : ',a %b)
---
+++ Python/PythonCoding/Daily Assignments/Xrange.py
@@ -0,0 +1,6 @@
+#Sum of numbers from 1 to 50 using range
+
+total =0
+for i in range(1, 51):
+    total += i
+print('Sum = :', total)
