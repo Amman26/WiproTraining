9add2fea648970a3fea34fe60798156406bf9f35
Amman26 <ammanfarooque@gmail.com>
2026-04-25 17:38:12 +0530

    Assignment Day1

---
+++ Python/PythonCoding/Daily Assignments/Day 1/Q5.py
@@ -0,0 +1,8 @@
+'''5. Write a program that takes a password as input and checks if it is strong (at least
+8 characters). Print a message based on the result.'''
+
+password = input('Enter Password: ')
+if len(password) >= 8:
+  print('Strong Password')
+else:
+  print('Weak Password')
