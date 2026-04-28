f96758fd3916436fb95aeb85fa181cd90d126d50
Amman26 <ammanfarooque@gmail.com>
2026-04-28 12:49:36 +0530

    filemgmt

---
+++ Python/PythonCoding/file_mgmt/csvfilemgmt.py
@@ -0,0 +1,38 @@
+import csv
+import os
+
+def write_csv(filename):
+    data = [
+        {'name': 'Amman','age': 23},
+        {'name': 'Farooque','age': 22}
+    ]
+
+    columnnames = ['name', 'age']
+
+    with open(filename, 'w',newline='\n') as file:
+        writer = csv.DictWriter()(file, fieldnames=columnnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+    print('CSV file written successfully')
+
+def read_csv(filename):
+    with open(filename, 'r', newline ='\n') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            print(f"Name: {row['name']}, Age : {row['age']}")
+
+def delete_csv(filename):
+    if os.path.exists(filename):
+        os.remove(filename)
+        print(f"{filename} delete successfully")
+    else:
+        print(f'{filename} does not exist')
+
+filename = "myfile.csv"
+
+write_csv(filename)
+print('Data read from CSV file:')
+read_csv(filename)
+#delete_csv(filename)
